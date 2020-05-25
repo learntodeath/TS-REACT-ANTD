f2b9d94fdd0eb2b7ba1493eafacc65da9410f6cf
敷衍 <37662205+learntodeath@users.noreply.github.com>
2020-05-25 17:24:45 +0800

    Add files via upload
    第二次提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from './components/Button/button'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button className='btn btn-lg' btnType='link' size='sm' href='www.baidu.com'>按钮</Button>
+      <Button className='btn btn-sm' btnType='primary'>按钮</Button>
+      <Button className='btn btn-primary' btnType='danger'>按钮</Button>
+      <Button className='btn btn-danger'>按钮</Button>
     </div>
   );
 }
